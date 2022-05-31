0e7a401707307c9d677b905755530366e0658af8
sraboni1998 <sraboni.mahbub@gmail.com>
2022-05-31 22:12:33 +0600

    Initial commit

---
+++ app/src/main/java/com/example/loaddatafromremoteserver/courseArrayList.java
@@ -0,0 +1,19 @@
+package com.example.loaddatafromremoteserver;
+
+public class courseArrayList {
+    String data;
+
+    public courseArrayList(String data) {
+        this.data = data;
+    }
+    public courseArrayList(){}
+
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String messages) {
+        this.data = data;
+    }
+}
